bf96bcca24326504f9c40ab4af979468ce6bf5aa
AYDEN CASIANO <121175005+AydenJC@users.noreply.github.com>
2022-12-21 13:50:54 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,82 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let SonyButton = document.querySelector('.option-one');
+let MicrosoftButton = document.querySelector('.option-two');
+let MakeagameButton = document.querySelector('.option-one.2');
+let MakeasystemButton = document.querySelector('.option-two.2');
+let MakeagamingconsoleButton = document.querySelector('.option-one.3');
+let WaitoneyearButton = document.querySelector('.option-two.3');
+let Makeagamingconsole = document.querySelector('.option-one.4');
+let Waitthreeyears = document.querySelector('.option-two.4');
+let WaitoneMonth = document.querySelector('.option-one.5');
+let MakeanewGame = document.querySelector('.option-two.5');
+let Wait = dcouument.querySelector('.option-one.6');
+let Makeagame = document.querySelector('option-two.6');
+let Wait10years = document.querySelector('option-one-ten');
+let Wait15years = document.querySelector('option -two-ten');
+let Wait20years = document.querySelector('option-three-ten');
+let Wait30years = document.querySelector('option-four-ten');
+
+SonyButton.onclick=function(){
+  optionthreescreen.style.display = 'block';
+  storyOpening.style.display = 'none';
+};
+
+MicrosoftButton.onclick=function(){
+  optionthreescreen.style.display = 'block';
+  storyOpening.style.display = 'none';
+};
+
+MakeaGame.onclick=function(){
+  optionthreescreen.style.display = "block";
+  optiononescreen.style.display = "none";
+};
+
+Makeasystem.onclick=function(){
+  optionthreescreen.style.display = 'block'
+  optiononescreen.style.display = 'none'
+};
+
+Makeaconsole.onclick=function(){
+  optionfivescreen.style.display = "block";
+  optionthreescreen.style.display = "none";
+};
+
+Waitthreeyear.onclick=function(){
+  optionsevenend.style.display = "block";
+  optionthreescreen.style.display = "none";
+};
+
+Waitamonth.onclick=function(){
+  optionfiveend.style.display = "block";
+  optionfivescreen.style.display = "none";
+};
+
+Makeanewgame.onclick=function(){
+  optionsevenscreen.style.display = "block";
+  optionfivescreen.style.display = "none";
+};
+
+Wait.onclick=function(){
+  optiononeend.style.display = "block";
+  optionsevenscreen.style.display = "none";
+};
+
+Makeagame.onclick=function(){
+  optionsixend.style.display = "block";
+  optionsevenscreen.style.display = "none";
+};
+
+Wait10years.onclick=function(){
+  optionfourend.style.display = "block";
+  optionsevenscreen.style.display = "none;";
+};
+
+Wait20years.onclick=function(){
+  optionthreeend.style.display = "block";
+  optionsevenscreen.style.display = "none";
+};
+
+Wait30years.onclick=fucntion(){
+  optiontwoend.style.display = "block";
+  optionsevenscreen.style.display ="none";
+};
